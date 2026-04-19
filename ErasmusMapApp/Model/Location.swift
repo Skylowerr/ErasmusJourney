5a311774d572f7f5f3ef6e24e18930e4150cccc1
Emirhan Gökçe <58822314+Skylowerr@users.noreply.github.com>
2026-04-19 17:55:56 +0200

    Update Location.swift

--- ErasmusMapApp/Model/Location.swift
+++ ErasmusMapApp/Model/Location.swift
@@ -9,7 +9,6 @@ import Foundation
 import MapKit
 
 struct Location : Identifiable, Equatable{
-//    let id = UUID().uuidString
     let name : String
     let cityName : String
     let coordinates : CLLocationCoordinate2D
@@ -17,15 +16,11 @@ struct Location : Identifiable, Equatable{
     let imageNames : [String]
     let link : String
     
-    //Uygulamayı kapatıp açsanız bile Roma'daki Kolezyum'un ID'si hep aynı kalır. UUID() her seferinde rastgele yeni bir değer üretir.
     var id : String{
         return name + cityName
     }
     
-    //TODO: Equatable. Fakat neden?
-    //Normalde Swift, iki tane karmaşık Location nesnesinin birbirine eşit olup olmadığını bilemez. Biz ona şu kuralı koyuyoruz: "Eğer iki lokasyonun ID'si aynıysa, bunlar aynı yerdir. Tüm açıklamayı veya resimleri tek tek kontrol etmene gerek yok."
-    //Default olarak swift, bütün verileri karşılaştırır. Ama benim için sadece ID lazım.
-    /**
+    /*
      I implemented Equatable to define a custom equality logic.
      Instead of comparing all properties, I compare only the id, because it uniquely identifies a location.
      This improves performance and ensures consistent identity checks, especially when working with SwiftUI lists.
